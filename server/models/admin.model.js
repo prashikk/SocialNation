4c11529a152b74a9e5b6e324c9d0ec8bf6a08455
Prashik Khotkar <91461422+prashikk@users.noreply.github.com>
2024-05-09 23:28:42 +0530

    adding changes

---
+++ server/models/admin.model.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const { ConnectionPoolClosedEvent } = require("mongodb");
+
+const adminSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 20,
+      validate: {
+        validator: function (value) {
+          return /^[a-zA-Z0-9]+$/.test(value);
+        },
+        message: function (props) {
+          return `${props.value} is not a valid username!`;
+        },
+      },
+    },
+
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      validate: {
+        validator: function (value) {
+          return value.length >= 6;
+        },
+        message: function (props) {
+          return "Password must be at least 6 characters long!";
+        },
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+adminSchema.pre("save", async function (next) {
+  const admin = this;
+  if (!admin.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    admin.password = await bcrypt.hash(admin.password, salt);
+    console.log(admin.password);
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+module.exports = mongoose.model("Admin", adminSchema);
